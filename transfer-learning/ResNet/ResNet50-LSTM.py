c5ce191a780d6657237c62229d404a417bcca9d9
ysun209 <ysun209@aucklanduni.ac.nz>
2024-09-09 10:14:11 +1200

    feat: Add MobileNetV4 Conv Small model for transfer learning training

--- transfer-learning/ResNet/ResNet50-LSTM.py
+++ transfer-learning/ResNet/ResNet50-LSTM.py
@@ -34,14 +34,14 @@ class ResNet_LSTM_Model(nn.Module):
         batch_size, seq_len, c, h, w = x.size()  # x is of shape (batch_size, num_frames, channels, height, width)
 
         # Process each frame through ResNet
-        features = []
+        resnet_features = []
         for t in range(seq_len):
             frame = x[:, t, :, :, :]  # Extract the t-th frame from each sequence
             features = self.resnet(frame)  # Pass each frame through Resnet
-            features.append(features)
+            resnet_features.append(features)
 
         # Stack the features along the time dimension
-        features = torch.stack(features, dim=1)  # Shape: (batch_size, num_frames, num_resnet_features)
+        resnet_features = torch.stack(resnet_features, dim=1)  # Shape: (batch_size, num_frames, num_resnet_features)
 
         # Initialize hidden and cell states
         batch_size = x.size(0)
@@ -49,7 +49,7 @@ class ResNet_LSTM_Model(nn.Module):
         c0 = torch.zeros(self.lstm.num_layers, batch_size, self.lstm.hidden_size).to(x.device)
         
         # Pass the sequence of features through the LSTM
-        lstm_out, (hn, cn) = self.lstm(features, (h0, c0))  # Shape of lstm_out: (batch_size, num_frames, lstm_hidden_size)
+        lstm_out, (hn, cn) = self.lstm(resnet_features, (h0, c0))  # Shape of lstm_out: (batch_size, num_frames, lstm_hidden_size)
 
         # Take the last output of the LSTM (or use mean pooling over time)
         lstm_last_output = lstm_out[:, -1, :]  # Shape: (batch_size, lstm_hidden_size)
@@ -148,3 +148,6 @@ for epoch in range(num_epochs):
 
     print(f"Epoch [{epoch+1}/{num_epochs}], Validation Loss: {avg_val_loss}")
     writer.add_scalar('Validation Loss', avg_val_loss, epoch)
+
+# Save model after training
+torch.save(model.state_dict(), f"models/{current_file_name}.pth")
