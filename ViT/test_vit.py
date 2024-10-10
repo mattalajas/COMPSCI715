7aab2d5d7d7de59a950f6ff313c5eb7988eb4124
kraw084 <kraw084@aucklanduni.ac.nz>
2024-10-11 12:35:40 +1300

    Added comments and docstrings to ViT

--- ViT/test_vit.py
+++ ViT/test_vit.py
@@ -11,29 +11,35 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import utils.data_utils as d_u
 from train_vit import evaluate_model
 
-
+#set image size and number of frames (for ViViT)
 img_size = 512
 frames = 12
 
+#function to reshape image batch tensors for ViVit models
 reshape_for_vivit = lambda x: x.transpose(0, 1)
-    
+
+#setup image validation augmentation (just formats image to right shape and size)
 x_transform = torchvision.transforms.Compose([
     torchvision.transforms.Resize((img_size, img_size)),
     reshape_for_vivit
 ])
 
+#control items the model is trained to predict
+col_pred = ["thumbstick_left_x", "thumbstick_left_y", "thumbstick_right_x", "thumbstick_right_y", "head_pos_x", "head_pos_y", "head_pos_z", "head_dir_a", "head_dir_b", "head_dir_c", "head_dir_d"]
+
+#setup validation and testsets
 val_sessions = d_u.DataUtils.read_txt("COMPSCI715/datasets/barbie_demo_dataset/val.txt")
-val_set = d_u.SingleGameDataset("Barbie", val_sessions, transform=x_transform, frame_count=frames)
+val_set = d_u.SingleGameDataset("Barbie", val_sessions, transform=x_transform, frame_count=frames, cols_to_predict=col_pred)
 
 test_sessions = d_u.DataUtils.read_txt("COMPSCI715/datasets/barbie_demo_dataset/test.txt")
-test_set = d_u.SingleGameDataset("Barbie", test_sessions, transform=x_transform, frame_count=frames)
+test_set = d_u.val_set = d_u.SingleGameDataset("Barbie", test_sessions, transform=x_transform, frame_count=frames, cols_to_predict=col_pred)
 
-
+#select device
 gpu_num = 3
 device = torch.device(f'cuda:{gpu_num}')
 print(f"Using device {gpu_num}: {torch.cuda.get_device_properties(gpu_num).name}")
 
-
+#create ViViT model object
 model = VideoViT(image_size = img_size,
                     image_patch_size = 64,
                     frame_patch_size = 2,
@@ -48,14 +54,15 @@ model = VideoViT(image_size = img_size,
                     frames = frames,
                     variant = "factorized_encoder").to(device)
 
+#put model in eval model and load weights
 model.eval()
-
 model_path = "models/vivit_v3/Epoch19.pt"
-
 model.load_state_dict(torch.load(model_path, weights_only=True))
 
+#compute validation loss
 loss = evaluate_model(model, val_set, 128, torch.nn.MSELoss(), device)
 print(f"Model val loss: {loss}")
 
+#compute test loss
 loss = evaluate_model(model, test_set, 128, torch.nn.MSELoss(), device)
 print(f"\nModel test loss: {loss}")
